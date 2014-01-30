17440165981c59ea0aeb72627c276a6354b85182
Dgizusse <rej.rioux@gmail.com>
2014-01-30 16:20:25 -0500

    - Config mode mostly done.

--- DickJohnson/DickJohnson.ino
+++ DickJohnson/DickJohnson.ino
@@ -1,115 +1,305 @@
-#define IN_PEDAL 0
-#define IN_VICE_PREASURE 5	//	Analog
-#define IN_PISTON_RANGE 4	//	Analog
-#define IN_VICE_OPEN 7
-
-#define OUT_CLOSE_VICE 8
-#define OUT_OPEN_VICE 13
-#define OUT_STOPPER_SERVO 11
-#define OUT_DROP_OIL 9
-#define OUT_PISTON_BACKWARD 10
-#define OUT_PISTON_FORWARD 12
-#define OUT_DEBUG_LIGHT 13
-
-#include <Servo.h>
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(1, 6, 5, 4, 3, 2);
-Servo stopperServo;
+#define MIN_ROD_SIZE 0.25f * ROD_SIZE_MULTIPLICATOR
+#define MAX_ROD_SIZE 1.25f * ROD_SIZE_MULTIPLICATOR
+#define ROD_SIZE_MULTIPLICATOR 2000
+
+#define MIN_EXTRUDE_LENGTH 1.0f * EXTRUDE_LENGTH_MULTIPLICATOR
+#define MAX_EXTRUDE_LENGTH 7.0f * EXTRUDE_LENGTH_MULTIPLICATOR
+#define EXTRUDE_LENGTH_MULTIPLICATOR 1250
+
+#define THREAD_NC true
+#define THREAD_NF false
+#define UNIT_MM true
+#define UNIT_INCH false
+#define UnitType bool
+#define ThreadType bool
+
+#define CONFIG_ENCODER_A 2
+#define CONFIG_ENCODER_A_INTERRUPT 0
+#define CONFIG_ENCODER_B 3
+
+#define IN_MODE_INIT 22
+#define IN_MODE_CONFIG 53
+#define IN_MODE_RUN 52
+
+#define OUT_MODE_CONFIG_LED 51
+#define OUT_MODE_RUN_LED 50
+#define OUT_MODE_INIT_LED 47
+
+#define IN_UNIT_SELECTOR 23
+#define IN_SET_EXTRUDE_LENGTH 48
+#define IN_SET_ROD_SIZE 49
+
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+#define max(a, b) (((a) < (b)) ? (b) : (a))
+#define clamp(in, low, high) min(max((in), (low)), (high))
+
+float extrusionTable[11][3] = {
+//	 Size		NC		NF
+	{0.25f,		1.0f,	1.0f},
+	{0.3125f,	1.0f,	1.0f},
+	{0.375,		1.0f,	1.0f},
+	{0.4375f,	1.0f,	1.0f},
+	{0.5f,		1.0f,	1.0f},
+	{0.625f,	1.0f,	1.0f},
+	{0.75f,		1.0f,	1.0f},
+	{0.875f,	1.0f,	1.0f},
+	{1.0f,		1.0f,	1.0f},
+	{1.125f,	1.0f,	1.0f},
+	{1.25f,		1.0f,	1.0f}
+};
+
+String sizeNames[11] = {
+	"  1/4",
+	" 5/16",
+	"  3/8",
+	" 7/16",
+	"  1/2",
+	"  5/8",
+	"  3/4",
+	"  7/8",
+	"1    ",
+	"1 1/8",
+	"1 1/4"
+};
+
+LiquidCrystal lcd(8, 9, 10, 11, 12, 7);
+
+int rodSize = 0.75f * ROD_SIZE_MULTIPLICATOR;
+int prevRodSize;
+bool canReadRodSize = false;
+
+int extrudeLength = 4.0f * EXTRUDE_LENGTH_MULTIPLICATOR;
+int prevExtrudeLength;
+bool canReadExtrudeLength = false;
+
+bool initialized = false;
+
+bool wasConfig = false;
+bool wasRun = false;
+bool wasInit = false;
+
+UnitType unitType = UNIT_MM;
+ThreadType threadType = THREAD_NC;
 
 void setup() {
-	pinMode(IN_PEDAL, INPUT);
-	digitalWrite(IN_PEDAL, true);
-	pinMode(IN_VICE_OPEN, INPUT);
-	digitalWrite(IN_VICE_OPEN, true);
-
-	pinMode(OUT_DEBUG_LIGHT, OUTPUT);
-	pinMode(OUT_CLOSE_VICE, OUTPUT);
-	pinMode(OUT_OPEN_VICE, OUTPUT);
-	pinMode(OUT_STOPPER_SERVO, OUTPUT);
-	pinMode(OUT_DROP_OIL, OUTPUT);
-	pinMode(OUT_PISTON_BACKWARD, OUTPUT);
-	pinMode(OUT_PISTON_FORWARD, OUTPUT);
-
-	stopperServo.attach(OUT_STOPPER_SERVO);
-	stopperServo.write(0);
-
 	lcd.begin(16, 2);
+	lcd.clear();
+	UpdateDisplayComplete();
+
+	SetupPin(CONFIG_ENCODER_A, true, true);
+	SetupPin(CONFIG_ENCODER_B, true, true);
+
+	SetupPin(IN_MODE_INIT, true, true);
+	SetupPin(IN_MODE_CONFIG, true, true);
+	SetupPin(IN_MODE_RUN, true, true);
+
+	SetupPin(OUT_MODE_INIT_LED, false);
+	SetupPin(OUT_MODE_RUN_LED, false);
+	SetupPin(OUT_MODE_CONFIG_LED, false);
+
+	SetupPin(IN_UNIT_SELECTOR, true, true);
+	SetupPin(IN_SET_EXTRUDE_LENGTH, true, true);
+	SetupPin(IN_SET_ROD_SIZE, true, true);
+
+	SetupPin(13, false);
+}
+
+void SetupPin(int pin, bool in) {
+	SetupPin(pin, in, false);
+}
+
+void SetupPin(int pin, bool in, bool pullUp) {
+	if (in) {
+		pinMode(pin, INPUT);
+		digitalWrite(pin, pullUp);
+	} else {
+		pinMode(pin, OUTPUT);
+	}
 }
 
 void loop() {
+	bool modeInit = !digitalRead(IN_MODE_INIT);
+	bool modeConfig = !digitalRead(IN_MODE_CONFIG);
+	bool modeRun = !digitalRead(IN_MODE_RUN);
+
+	if (modeInit && !modeConfig && !modeRun) {
+		if (!wasInit) {
+			wasRun = wasConfig = false;
+			wasInit = true;
+			digitalWrite(OUT_MODE_CONFIG_LED, false);
+			digitalWrite(OUT_MODE_RUN_LED, false);
+			digitalWrite(OUT_MODE_INIT_LED, true);
+
+			if (canReadRodSize || canReadExtrudeLength) {
+				detachInterrupt(CONFIG_ENCODER_A_INTERRUPT);
+				canReadExtrudeLength = canReadRodSize = false;
+			}
+		}
+		LoopInit();
+	} else if (!initialized) {
+		//	Can't go into another mode if not initialized. Display an error message on screen.
+		lcd.clear();
+		lcd.print("Select INIT mode");
+	} else if (modeConfig && !modeRun && !modeInit) {
+		if (!wasConfig) {
+			wasRun = wasInit = false;
+			wasConfig = true;
+			digitalWrite(OUT_MODE_INIT_LED, false);
+			digitalWrite(OUT_MODE_RUN_LED, false);
+			digitalWrite(OUT_MODE_CONFIG_LED, true);
+			canReadRodSize = canReadExtrudeLength = false;
+		}
+		LoopConfig();
+	} else if (!modeConfig && modeRun) {
+		if (!wasRun) {
+			wasConfig = wasInit = false;
+			wasRun = true;
+			digitalWrite(OUT_MODE_INIT_LED, false);
+			digitalWrite(OUT_MODE_RUN_LED, true);
+			digitalWrite(OUT_MODE_CONFIG_LED, false);
+
+			if (canReadRodSize || canReadExtrudeLength) {
+				detachInterrupt(CONFIG_ENCODER_A_INTERRUPT);
+				canReadExtrudeLength = canReadRodSize = false;
+			}
+		}
+		LoopRun();
+	}
+}
+
+void LoopInit() {
+	ZeroCompleted();
+}
+
+void ZeroCompleted() {
+	initialized = true;
+}
+
+void LoopConfig() {
+	bool updateDisplay = false;
+
+	bool setSize = !digitalRead(IN_SET_ROD_SIZE);
+	bool setLength = !digitalRead(IN_SET_EXTRUDE_LENGTH);
+
+	UnitType setUnit = !digitalRead(IN_UNIT_SELECTOR);
+
+	if (prevRodSize != rodSize) {
+		updateDisplay = true;
+		prevRodSize = rodSize;
+		UpdateDisplayRodSize();
+	}
+
+	if (prevExtrudeLength != extrudeLength) {
+		updateDisplay = true;
+		prevExtrudeLength = extrudeLength;
+		UpdateDisplayExtureLength();
+	}
+
+	if (setUnit != unitType) {
+		updateDisplay = true;
+		unitType = setUnit;
+
+		UpdateDisplayRodSize();
+		UpdateDisplayExtureLength();
+	}
+
+	bool setRodSizeButton = !digitalRead(IN_SET_ROD_SIZE);
+	bool setExtrudeLengthButton = !digitalRead(IN_SET_EXTRUDE_LENGTH);
+
+	if (canReadRodSize && !setRodSizeButton) {
+		detachInterrupt(CONFIG_ENCODER_A_INTERRUPT);
+		canReadRodSize = false;
+	} else if (!canReadRodSize && setRodSizeButton) {
+		attachInterrupt(CONFIG_ENCODER_A_INTERRUPT, UpdateRodSizeInterrupt, CHANGE);
+		canReadRodSize = true;
+	} else if (canReadExtrudeLength && !setExtrudeLengthButton) {
+		detachInterrupt(CONFIG_ENCODER_A_INTERRUPT);
+		canReadExtrudeLength = false;
+	} else if (!canReadExtrudeLength && setExtrudeLengthButton) {
+		attachInterrupt(CONFIG_ENCODER_A_INTERRUPT, UpdateExtrudeLengthInterrupt, CHANGE);
+		canReadExtrudeLength = true;
+	}
+}
+
+void LoopRun() {
+	
+}
+
+//	Interupt
+void PistonPositionInterrupt() {
+
+}
+
+void UpdateRodSizeInterrupt() {
+	if (digitalRead(CONFIG_ENCODER_A) == digitalRead(CONFIG_ENCODER_B)) {
+		rodSize = min(rodSize + 1, MAX_ROD_SIZE);
+	} else {
+		rodSize = max(rodSize - 1, MIN_ROD_SIZE);
+	}
+}
+
+void UpdateExtrudeLengthInterrupt() {
+	if (digitalRead(CONFIG_ENCODER_A) == digitalRead(CONFIG_ENCODER_B)) {
+		extrudeLength = min(extrudeLength + 1, MAX_EXTRUDE_LENGTH);
+	} else {
+		extrudeLength = max(extrudeLength - 1, MIN_EXTRUDE_LENGTH);
+	}
+}
+
+//	Display
+void UpdateDisplayComplete() {
 	lcd.clear();
-	lcd.print("Wait pedal!");
-	// NOT on because of PullUp resistor.
-	if (!digitalRead(IN_PEDAL)) {
-		//	Close vice.
-		digitalWrite(OUT_CLOSE_VICE, true);
 
-				
-		lcd.clear();
-		// Wait for target preasure.
-		lcd.print("Wait preasure.");
-		while (analogRead(IN_VICE_PREASURE) < 512) {
-			delay(5);
-		}
-		digitalWrite(OUT_CLOSE_VICE, false);
-		
-		lcd.clear();
-		lcd.print("Dropping oil!");
-		//	Drop some oil!
-		digitalWrite(OUT_DROP_OIL, true);
-		delay(500);
-		digitalWrite(OUT_DROP_OIL, false);
-		
-		lcd.clear();
-		lcd.print("Wait Piston back.");
-		//	Move piston back a little to prevent scratching noise.... Atchoum!
-		digitalWrite(OUT_PISTON_BACKWARD, true);
-		
-		while (analogRead(IN_PISTON_RANGE) > 250) {
-			delay(1);
-		}
-		digitalWrite(OUT_PISTON_BACKWARD, false);
-		
-		lcd.clear();
-		lcd.print("Raising Stopper.");
-		//	Raise Stopper
-		stopperServo.write(179);
-		delay(250);
-		
-		lcd.clear();
-		lcd.print("EXTRUDING!!!");
-		lcd.setCursor(0, 1);
-		lcd.print("Move forward.");
-		//	EXTRUDE!!!
-		digitalWrite(OUT_PISTON_FORWARD, true);
-		while (analogRead(IN_PISTON_RANGE) < 900) {
-			delay(1);
-		}
-		digitalWrite(OUT_PISTON_FORWARD, false);
+	lcd.print("D:      L:");
+}
 
-		lcd.clear();
-		lcd.print("Move backward.");
-		//	Move back.
-		digitalWrite(OUT_PISTON_BACKWARD, true);
-		while (analogRead(IN_PISTON_RANGE) > 250) {
-			delay(1);
-		}
-		digitalWrite(OUT_PISTON_BACKWARD, false);
+void UpdateDisplayRodSize() {
+	float fSize = (float)rodSize / (float)ROD_SIZE_MULTIPLICATOR;
 
-		lcd.clear();
-		lcd.print("Lowering Stopper");
-		//	Lower Stopper.
-		stopperServo.write(0);
-
-		lcd.clear();
-		lcd.print("Open vice");
-		//	Open vice.
-		digitalWrite(OUT_OPEN_VICE, true);
-		// Wait for vice open micro switch.
-		while (digitalRead(IN_VICE_OPEN)) {
-			delay(5);
+	int closestIndex = -1;
+	float closest = 999.0f;
+	String result = (String)rodSize;
+	for (int i=0; i<11; i++) {
+		float dist = extrusionTable[i][0] - fSize;
+		if (dist < 0.0f) {
+			dist *= -1.0f;
 		}
-		digitalWrite(OUT_OPEN_VICE, false);
+
+		if (dist < closest) {
+			closest = dist;
+			closestIndex = i;
+		}
+	}
+
+	if (closestIndex >= 0) {
+		result = sizeNames[closestIndex];
+	}
+
+	lcd.setCursor(2, 0);
+	lcd.print(result);
+}
+
+void UpdateDisplayExtureLength() {
+	lcd.setCursor(10, 0);
+
+	float fLength = (float)extrudeLength / (float)EXTRUDE_LENGTH_MULTIPLICATOR;
+
+	if (unitType == UNIT_MM) {
+		fLength *= 25.4f;
+
+		String result = "";
+		if (fLength < 10) {
+			result = "00";
+		} else if (fLength < 100) {
+			result = "0";
+		}
+		lcd.print(result + (String)(int)fLength + "mm");
+	} else {
+		int major = extrudeLength / EXTRUDE_LENGTH_MULTIPLICATOR;
+		int minor = 100 * ((float)(extrudeLength % EXTRUDE_LENGTH_MULTIPLICATOR) / (float)EXTRUDE_LENGTH_MULTIPLICATOR);
+
+		lcd.print((String)major + "." + (minor<10?("0" + (String)minor):(String)minor) + "\"");
 	}
 }
